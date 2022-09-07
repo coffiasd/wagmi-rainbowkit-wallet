c88e431b85372fdba7ed7995df99e3806e86394c
coffiasd <759291502@qq.com>
2022-09-07 15:20:05 +0800

    fix bug

--- components/Header.js
+++ components/Header.js
@@ -15,29 +15,29 @@ export default function Header() {
     console.log(address);
 
     return (
-        <div class="navbar bg-base-100">
-            <div class="flex-1">
-                <a class="btn btn-ghost normal-case text-xl">DaisyUI</a>
-                <a target="_blank" class="text-base ml-3 mt-1 font-medium" href="https://twitter.com/coffiasse">Twitter</a>
-                <a target="_blank" class="text-base ml-6 mt-1 font-medium" href="https://github.com/coffiasd">Github</a>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">DaisyUI</a>
+                <a target="_blank" rel="noreferrer" className="text-base ml-3 mt-1 font-medium" href="https://twitter.com/coffiasse">Twitter</a>
+                <a target="_blank" rel="noreferrer" className="text-base ml-6 mt-1 font-medium" href="https://github.com/coffiasd">Github</a>
             </div>
 
-            <div class="navbar-start">
+            <div className="navbar-start">
             </div>
 
-            <div class="navbar-end">
-                <button class="btn btn-ghost btn-circle">
-                    <div class="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                        <span class="badge badge-xs badge-primary indicator-item"></span>
+            <div className="navbar-end">
+                <button className="btn btn-ghost btn-circle">
+                    <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+                        <span className="badge badge-xs badge-primary indicator-item"></span>
                     </div>
                 </button>
 
 
                 {isConnected ?
-                    (<><button class="btn btn-sm btn-outline btn-primary ml-3 normal-case" onClick={openAccountModal}>Profile</button><button class="btn btn-sm btn-outline btn-primary ml-3 normal-case" onClick={openChainModal}>Chain</button></>)
+                    (<><button className="btn btn-sm btn-outline btn-primary ml-3 normal-case" onClick={openAccountModal}>Profile</button><button className="btn btn-sm btn-outline btn-primary ml-3 normal-case" onClick={openChainModal}>Chain</button></>)
                     :
-                    (<button class="btn btn-sm btn-outline btn-primary ml-3 normal-case" onClick={openConnectModal}>connect wallet</button>)
+                    (<button className="btn btn-sm btn-outline btn-primary ml-3 normal-case" onClick={openConnectModal}>connect wallet</button>)
                 }
             </div>
         </div >
--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,24 @@ export default function Home() {
 
       <Header />
 
-      <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content flex-col lg:flex-row-reverse">
-          <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
           <div>
-            <h1 class="text-5xl font-bold">Wallets Connect Demo!</h1>
-            <p class="py-6">This is wallet connect demo page created by ayden lee.If you get any problems when using this demo, feel free to connect me!</p>
-            <label for="my-modal" class="btn btn-primary modal-button">Get Started</label>
+            <h1 className="text-5xl font-bold">Wallets Connect Demo!</h1>
+            <p className="py-6">This is wallet connect demo page created by ayden lee.If you get any problems when using this demo, feel free to connect me!</p>
+            <label htmlFor="my-modal" className="btn btn-primary modal-button">Get Started</label>
           </div>
         </div>
       </div>
 
-      <input type="checkbox" id="my-modal" class="modal-toggle" />
-      <div class="modal">
-        <div class="modal-box">
-          <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
-          <p class="py-4">Click the connect button on the top of this page!</p>
-          <div class="modal-action">
-            <label for="my-modal" class="btn">Yay!</label>
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+          <p className="py-4">Click the connect button on the top of this page!</p>
+          <div className="modal-action">
+            <label htmlFor="my-modal" className="btn">Yay!</label>
           </div>
         </div>
       </div>
